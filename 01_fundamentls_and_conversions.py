26afeac462764dd6bf486df41361e81b7f9126e1
Kathe Vinay <vinaykathe904@gmail.com>
2026-09-15 22:19:47 +0530

    Update 01_fundamentls_and_conversions.py

--- 01_fundamentls_and_conversions.py
+++ 01_fundamentls_and_conversions.py
@@ -73,5 +73,50 @@ if num>0:
     print("given number is positive")
 else:
     print("given number is negative")
+
+
+# mini calculator using functions
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+
+print("select operation.")
+print("1.add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+
+while(True):
+    choice = input("enter choice 1/2/3/4")
+
+    if choice in('1' '2' '3' '4' '5'):
+       try:
+           num1 = float(input("enter the 1st number:"))
+           num2 = float(input("enter the 2nd number:"))
+       except ValueError:
+           print("invalid input.please enter a number")
+           continue
+       if choice=='1':
+           print(num1,"+",num2, "=",add(num1,num2))
+       elif choice=='2':
+         print(num1,"-",num2,"=",sub(num1,num2))
+       elif choice=='3':
+           print(num1,"*",num2,"=",multiply(num1,num2))
+       elif choice=="4":
+           print(num1,"/",num2,"=",divide(num1,num2))    
+
+       next_calculation = input("lets do calculation?(yes/no)")
+       if next_calculation=="no":
+         break
+    else:
+        print("invalid input")   
+
+ 
+ 
     
 
